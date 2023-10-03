9d1f864e26df30803475d91543626b328a7d82bb
kittima1721 <milw.mael@gmail.com>
2023-10-03 14:47:46 +0700

    dashboard

--- controller/admin/dashboard.go
+++ controller/admin/dashboard.go
@@ -26,15 +26,24 @@ func Dashboard(c *gin.Context) {
 		Name  string `json:"name"`
 		Value int    `json:"value"`
 	}
-	var result pieRead
-	db.Conn.Raw("SELECT categories.id as id,categories.name as name ,COUNT(products.id) as value from products JOIN categories on products.category_id = categories.id WHERE products.deleted_at is null GROUP by products.category_id").Scan(&result)
+	var result []pieRead
+	var id uint
+	var name string
+	var value int
+	row := db.Conn.Raw("SELECT categories.id as id,categories.name as name ,COUNT(products.id) as value from products JOIN categories on products.category_id = categories.id WHERE products.deleted_at is null GROUP by products.category_id").Row()
+	row.Scan(&id, &name, &value)
+	result = append(result, pieRead{
+		ID:    id,
+		Name:  name,
+		Value: value,
+	})
 
-	type Chart struct {
+	type chart struct {
 		ID    uint   `json:"id"`
 		Name  string `json:"name"`
 		Value int    `json:"value"`
 	}
-	var r Chart
+	var r []chart
 	db.Conn.Raw("SELECT DATE_FORMAT(ot.created_at,'%M') as name,SUM(ot.quantity*p.price) as value,DATE_FORMAT(ot.created_at,'%m') as id FROM order_items as ot JOIN products as p ON ot.product_id=p.id GROUP BY DATE_FORMAT(created_at,'%M')").Scan(&r)
 	c.JSON(http.StatusOK, gin.H{"total_price": humanize.Commaf(float64(totalPrice)),
 		"count_store":   humanize.Commaf(float64(cstore)),
