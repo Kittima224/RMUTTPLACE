86f7bd6728d2629db63993eef2852df9b2501e6c
kittima1721 <milw.mael@gmail.com>
2023-10-11 04:23:28 +0700

    cc

--- controller/admin/dashboard.go
+++ controller/admin/dashboard.go
@@ -39,17 +39,17 @@ func Dashboard(c *gin.Context) {
 	}
 	var r []Chart
 	var cc []Chart
-	db.Conn.Raw("SELECT to_char(ot.created_at,'MON') as x,sum(ot.quantity*p.price) as y FROM order_items as ot JOIN products as p on ot.product_id=p.id GROUP BY to_char(ot.created_at,'MON') order by to_char(ot.created_at,'MM')").Scan(&r)
+	db.Conn.Raw("select sum(ot.quantity*p.price) as y,to_char(ot.created_at,'MON') as x from order_items as ot JOIN products as p on ot.product_id=p.id group by to_char(ot.created_at,'MON')").Scan(&r)
+
 	for _, c := range r {
 		cc = append(cc, Chart{
 			X: c.X,
-
 			Y: c.Y,
 		})
 	}
 	var j []Chart
 	var gg []Chart
-	db.Conn.Raw("select sum(ot.quantity*p.price) as y,to_char(ot.created_at,'MON') as x from order_items as ot JOIN products as p on ot.product_id=p.id group by to_char(ot.created_at,'MON')").Scan(&j)
+	db.Conn.Raw("SELECT to_char(ot.created_at,'MON') as x,sum(ot.quantity*p.price) as y FROM order_items as ot JOIN products as p on ot.product_id=p.id GROUP BY to_char(ot.created_at,'MON') order by to_char(ot.created_at,'MM')").Scan(&j)
 	for _, g := range j {
 		gg = append(gg, Chart{
 			X: g.X,
